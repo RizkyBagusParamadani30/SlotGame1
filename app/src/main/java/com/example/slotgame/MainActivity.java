74011c169a3737d8e9be64720e1c0c7527d60cb6
kikik <kikikun71@gmail.com>
2023-04-11 19:12:43 +0700

    Slot Game Belum dialog box

--- app/src/main/java/com/example/slotgame/MainActivity.java
+++ app/src/main/java/com/example/slotgame/MainActivity.java
@@ -11,18 +11,23 @@ import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
-    Thread t; /* Nama untuk sebuah thread*/
+    Thread t1;
+    Thread t2;
+    Thread t3; /* Nama untuk sebuah thread*/
     Handler h; /*Membutuhkan sebuah handler*/
     int id = 1; /*Untuk Gambar Pertama*/
-    private ImageView imgSlot;
+    private ImageView imgSlot1;
     private Button btnStarStop;
+    private ImageView imgSlot2;
+    private ImageView imgSlot3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        this.imgSlot = this.findViewById(R.id.imgSlot);
+        this.imgSlot1 = this.findViewById(R.id.imgSlot1);
+        this.imgSlot2 = this.findViewById(R.id.imgSlot2);
+        this.imgSlot3 = this.findViewById(R.id.imgSlot3);
         this.btnStarStop = this.findViewById(R.id.btnStartStop);
         this.btnStarStop.setOnClickListener(this);
 
@@ -31,12 +36,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
     @Override
     public void onClick(View v) { /* Button */
-        if (this.t != null && this.t.isAlive()){
-            this.t.interrupt();
+        if (t1 != null && t1.isAlive()){
+            t1.interrupt();
+            return;
+        }
+        if (t2 != null && t2.isAlive()){
+            t2.interrupt();
+            return;
+        }
+        if (t3 != null && t3.isAlive()){
+            t3.interrupt();
             return;
         }
 
-        this.t = new Thread(new Runnable() {
+        this.t1 = new Thread(new Runnable() {
             @Override
             public void run() {
                 /*Kode Disini dijalankan di Thread terpisah*/
@@ -49,7 +62,8 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                             @Override
                             public void run() {
                                 /*Proses berjalan di dalam UI Thread */
-                                imgSlot.setImageResource(Helper.getIcon(id));/*Ambil dari helper untuk mengambil gambar*/
+                                imgSlot1.setImageResource(Helper.getIcon(id));/*Ambil dari helper untuk mengambil gambar*/
+
                             }
                         });
                     } catch (InterruptedException e) {
@@ -58,6 +72,53 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 }
             }
         });
-        this.t.start();
+        this.t1.start();
+        t2 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                /*Kode Disini dijalankan di Thread terpisah*/
+                while (true){
+                    if (id==9)id =1; /* Jka id sampai 9 maka jadikan id menjadi 1 */
+                    else id++; /* Jika tidak id ++*/
+                    try {
+                        Thread.sleep(100); /* Tidurkan thread berikut selama 100 ms*/
+                        h.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                /*Proses berjalan di dalam UI Thread */
+                                imgSlot2.setImageResource(Helper.getIcon(id));/*Ambil dari helper untuk mengambil gambar*/
+                            }
+                        });
+                    } catch (InterruptedException e) {
+                        break;
+                    }
+                }
+            }
+        });
+        t2.start();
+
+        t3 = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                /*Kode Disini dijalankan di Thread terpisah*/
+                while (true){
+                    if (id==9)id =1; /* Jka id sampai 9 maka jadikan id menjadi 1 */
+                    else id++; /* Jika tidak id ++*/
+                    try {
+                        Thread.sleep(100); /* Tidurkan thread berikut selama 100 ms*/
+                        h.post(new Runnable() {
+                            @Override
+                            public void run() {
+                                /*Proses berjalan di dalam UI Thread */
+                                imgSlot3.setImageResource(Helper.getIcon(id));/*Ambil dari helper untuk mengambil gambar*/
+                            }
+                        });
+                    } catch (InterruptedException e) {
+                        break;
+                    }
+                }
+            }
+        });
+        t3.start();
     }
 }
